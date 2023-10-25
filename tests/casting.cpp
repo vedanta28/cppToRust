b80cb4fcd373d2520afa2507ad217169653c3ee6
Vedanta Mohapatra <74861233+vedanta28@users.noreply.github.com>
2023-10-25 19:54:21 +0530

    explicit casting test added

---
+++ tests/casting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x = 5;
+  int t = (int)x;
+  long long int a = (long long int)x;
+  unsigned long int l = (unsigned long int) x;
+  int l = (int)((int)x);
+}
